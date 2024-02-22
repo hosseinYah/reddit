4e833e6b50e739e2fa220e65a4575028008cd9f3
Hossein <hossein@xeleva.com>
2024-02-21 18:44:06 -0600

    multiple menu added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  // Hooks
+  const [country, setCountry] = useState('') 
+
+  const handleCountry = (event) => {
+    setCountry(event.target.value)
+    // console.log(event.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <div> Please choose an option</div>
+        
+        <FormControl sx={{ m: 1, minWidth: 200 }}>
+        <InputLabel>Country</InputLabel>
+        <Select
+          autoWidth
+          value={country}
+          label="Country"
+          onChange={handleCountry}
+        > 
+          <MenuItem value={'mexico'}>México</MenuItem>
+          <MenuItem value={'finland'}>Finaland</MenuItem>
+        </Select>
+        
+      </FormControl>
+
       </header>
     </div>
   );
